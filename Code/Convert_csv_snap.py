63ec9710e7a44004421fcbdc6921a358909804e4
7gsingh <123275679+7gsingh@users.noreply.github.com>
2023-04-18 22:53:33 -0500

    Add files via upload

---
+++ Code/Convert_csv_snap.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 18:40:48 2023
+
+@author: 7sing
+"""
+
+import pandas as pd
+import numpy as np
+import glob as glob
+import matplotlib.pyplot as plt
+
+path = 'D:/Dr_Le/DNN_Results_200snap_2D' ## path to the folder where the files are##
+
+csv_files = glob.glob(path + '/*.csv')
+
+Snapshots = len(csv_files) #Snapshots
+
+count = 0
+
+No_of_elements = 73728 ## Total number of elements in the file, 384x192= 73728 ##
+
+Rows = 384
+
+Col = 192
+
+Components = 2
+
+U = np.empty((No_of_elements,))
+
+V = np.empty((No_of_elements,))
+
+U_act = np.empty((Rows,Col,Snapshots))
+
+V_act = np.empty((Rows,Col,Snapshots))
+
+X = np.empty((Snapshots,Rows,Col,Components))
+
+for file in csv_files:
+    df = pd.read_csv(file, usecols=['Ucat:0','Ucat:1'])
+    U[:] = df['Ucat:0']
+    V[:] = df['Ucat:1']
+    U_act[:,:,count] = np.reshape(U, (Rows,Col))
+    V_act[:,:,count] = np.reshape(V, (Rows,Col))
+    count = count + 1
+
+
+######Reading data into the output variable######
+
+
+for i in range(0,Snapshots):
+    X[i,:,:,0] = U_act[:,:,i]
+    X[i,:,:,1] = V_act[:,:,i]
+
+
+
+#####Plotting a sample of the data#####
+
+
+X_axis = range(0,192)
+Y_axis = range(0,384)
+
+[X_plot,Y_plot] = np.meshgrid(X_axis,Y_axis)
+
+fig, ax = plt.subplots(1,1)    
+ax.contour(X_plot,Y_plot,X[180,:,:,0])
